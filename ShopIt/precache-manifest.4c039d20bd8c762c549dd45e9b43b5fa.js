a43871c8eb3c79c79e06f2c29794c575cf5fe375
Zain Hasib <zainhasib@gmail.com>
2018-10-24 16:55:31 +0530

    Done ShopIt

---
+++ ShopIt/precache-manifest.4c039d20bd8c762c549dd45e9b43b5fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d117d4f2dedf8357c8ea",
+    "url": "/static/js/main.d117d4f2.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "d117d4f2dedf8357c8ea",
+    "url": "/static/css/main.3d4b30d8.chunk.css"
+  },
+  {
+    "revision": "af1ac8554d8b2f483d3806de4e206723",
+    "url": "/index.html"
+  }
+];
